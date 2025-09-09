93af826bdd1bfb1a53aee2fc9bd3298c2deb04cf
anac2828 <anac2828@yahoo.com>
2025-09-08 17:31:31 -0700

    ready to eploy

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 const themeSwitcher = document.querySelector('.theme-switcher');
 let activeTheme = localStorage.getItem('theme');
 
-themeSwitcher.addEventListener('click', e => {
-  if (e.target.tagName != 'INPUT') {
-    return;
-  }
-
-  const themeToActivate = e.target.id;
-  changeTheme(themeToActivate);
-});
-
+// FUNCTION TO CHANGE THEME
 function changeTheme(theme) {
+  // Add a class to the body to enable color theme transition
   document.body.classList.add('theme-switch');
   if (theme === 'system') {
     localStorage.removeItem('theme');
     document.body.removeAttribute('data-theme');
   } else {
+    // 'data-theme' attributed styles are defined in CSS - see _root.scss file
     localStorage.setItem('theme', theme);
     document.body.setAttribute('data-theme', theme);
   }
 
   document.body.addEventListener(
+    // The transitionend event is fired when a CSS transition has completed.
+    // Remove the class after the transition is done
     'transitionend',
     () => {
       document.body.classList.remove('theme-switch');
@@ -29,8 +25,20 @@ function changeTheme(theme) {
   );
 }
 
+// EVENT LISTENER
+themeSwitcher.addEventListener('click', (e) => {
+  if (e.target.tagName != 'INPUT') {
+    return;
+  }
+  // will be 'light', 'dark', or 'system'
+  const themeToActivate = e.target.id;
+  changeTheme(themeToActivate);
+});
+
+// ON LOAD gets the active theme from local storage
 window.onload = () => {
   if (activeTheme) {
+    // Set the correct radio button to checked depeinding on the active theme
     document.getElementById(activeTheme).checked = true;
     changeTheme(activeTheme);
   }
